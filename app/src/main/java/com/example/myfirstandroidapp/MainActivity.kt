34485be74541c189379e2715f92f6e3bcecce1e1
HP <madaminochilov08@gmail.com>
2022-10-01 17:09:21 +0500

    Homework

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -2,21 +2,82 @@ package com.example.myfirstandroidapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirstandroidapp.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityMainBinding
+    private val nothing = 0
+    private val multiply = 5
+    private val plus = 1
+    private val minus = 2
+    private val div = 3
+    private val percent = 4
+
+
+    var mode = nothing
+    val resultCalculator = 0
+    var secondNumber = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.Button0.setOnClickListener {
+
+            if (binding.display.text.length != 1)
+
+                binding.display.append("1")
+            else {
+                binding.display.text = "1"
+            }
+        }
+
+        binding.btnEqual.setOnClickListener {
+            when(mode){
+                multiply-> {
+                    val firstNumber = binding.display.text.toString().toLong()
+                    val secondNumber = secondNumber.toLong()
+
+                    binding.display.text = (firstNumber * secondNumber).toString()
+
+                }
+            }
+        }
+
+        binding.Button1.setOnClickListener {
+
+
+            when (mode) {
+                nothing -> {
+                    if (binding.display.text != "0")
+
+                        binding.display.append("1")
+                    else {
+                        binding.display.text = "1"
+                    }
+
+                }
+                else -> secondNumber = secondNumber.plus(1)
 
 
 
+            }
+            binding.Buttonx.setOnClickListener {
+                mode = multiply
+            }
 
 
+            fun allClearAction() {}
+            fun backSpaceAction() {}
+            fun equalAction() {}
+        }
     }
-
-    fun allClearAction() {}
-    fun backSpaceAction() {}
-    fun equalAction() {}
 }

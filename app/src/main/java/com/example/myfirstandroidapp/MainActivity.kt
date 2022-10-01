0a25fad041652c73061bf382b65f17237da8a11d
HP <madaminochilov08@gmail.com>
2022-10-01 15:03:09 +0500

    initial commit

---
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myfirstandroidapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+    fun allClearAction() {}
+    fun backSpaceAction() {}
+    fun equalAction() {}
+}
